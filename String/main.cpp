65a7da904a7e460e6244f3a5bbed1f4dd8cdb3f2
Zhenggang Lin <3190916418@qq.com>
2023-04-06 15:08:20 +0800

    Initial Commit

---
+++ String/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  String
+//
+//  Created by 木林 on 2023/4/6.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
